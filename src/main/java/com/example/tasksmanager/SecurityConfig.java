d8600906cfb4f85c7109d468f784d455367221d3
Anatoliy <mafik_tolik@mail.ru>
2024-02-16 19:27:37 +0300

    hm7 part1

---
+++ src/main/java/com/example/tasksmanager/SecurityConfig.java
@@ -0,0 +1,39 @@
+package com.example.tasksmanager;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(securedEnabled = true, jsr250Enabled = true)
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry ->
+                        authorizationManagerRequestMatcherRegistry
+                                .requestMatchers("/executors/**").hasRole("ADMIN")
+                                .requestMatchers("/tasks/all").hasAnyRole("ADMIN", "USER")
+                                .requestMatchers("/tasks/add").hasAnyRole("ADMIN", "USER")
+                                .requestMatchers("/tasks/status/*").hasAnyRole("ADMIN", "USER")
+                                .requestMatchers("/tasks/*/add-executor").hasAnyRole("ADMIN", "USER")
+                                .requestMatchers("/tasks/*/assign-executor").hasAnyRole("ADMIN", "USER")
+                                .requestMatchers("/tasks/*/update-status").hasAnyRole("ADMIN", "USER")
+                                .requestMatchers("/tasks/*/delete").hasRole("ADMIN")
+                                .requestMatchers("/login/**").permitAll()
+                                .anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(httpSecuritySessionManagementConfigurer ->
+                        httpSecuritySessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        return http.build();
+    }
+}
--- src/main/java/com/example/tasksmanager/TaskController.java
+++ src/main/java/com/example/tasksmanager/TaskController.java
@@ -3,6 +3,7 @@ package com.example.tasksmanager;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
